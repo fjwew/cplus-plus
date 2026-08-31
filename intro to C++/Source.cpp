33480f26da2977ffd8a970ac29b124aa01b4ed10
CODER <CODER@A26-PC6>
2026-08-31 16:21:22 +0300

    Add project files.

---
+++ intro to C++/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+// \n - перехід на новий рядок
+// \t - табуляція
+// \\ - \деяка інформація\
+// \' - 'деяка інформація'
+// \" - "деяка інформація"
+
+void main()
+{
+	// i love you c++
+
+	cout << "I\n";
+	cout << "\tlove\n";
+	cout << "\t\tyou\n";
+	cout << "\t\t\tC++!\n";
+
+	// resume
+
+	cout << "\t\t...:::RESUME:::...\n";
+	cout << "Name:       \t\t\tIvan\n";
+	cout << "Surname:     \t\t\tIvanov\n";
+	cout << "Last name:   \t\t\tIvanovich\n";
+	cout << "Date of birth:\t\t\t22.06.06\n";
+	cout << "City:        \t\t\tRivne\n";
+	cout << "Age:         \t\t\t17\n";
+	cout << "Hobby:       \t\t\tC++\n";
+	cout << "::.....................................;;::\n";
+
+	// every hunter wants to know
+
+	cout << "Every\n";
+	cout << "\thunter\n";
+	cout << "\t\twants\n";
+	cout << "\t\t\tto\n";
+	cout << "\t\t\t\tknow\n";
+	cout << "\t\t\t\t\twhere\n";
+	cout << "\t\t\t\t\t\tthe\n";
+	cout << "\t\t\t\t\t\t\tpheasant\n";
+	cout << "\t\t\t\t\t\t\t\tis.\n";
+}
+
+int number()
+{
+	cout << "12345";
+	cout << "56789";
+
+	return 0;
+}
